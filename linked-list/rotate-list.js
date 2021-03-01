6e090df8d07fc15ee46379ebeab5969eee6e8903
moamlrh <moamll288@gmail,com>
2021-03-01 21:07:51 +0300

    edit rotate linked list

--- linked-list/rotate-list.js
+++ linked-list/rotate-list.js
@@ -1,27 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function Node(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ *
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
 class Node {
     constructor(val, next) {
         this.val = val === undefined ? 0 : val;
         this.next = next === undefined ? null : next;
     }
 }
-
-var rotateList = function (head, k) {
-    if (!head || !head.next || k < 0) return head;
-    var tail = curr = head;
-    var count = 1;
-    while (tail.next) { // get the last node in list [tail]
-        count++ // and at the same time, to get the length of list [head]
-        tail = tail.next
+var rotateRight = function (head, k) {
+    if (head === null || head.next === null || k === 0) return head;
+    let length = 0;
+    let cur = head;
+    let tail = null;
+    while (cur) {
+        length++;
+        if (cur.next === null) tail = cur;
+        cur = cur.next;
     }
-    tail.next = head; // add the head next to the tail
-    if (k % count === 0) return head; // if the list length = to k that's mean will return  the same head
-    var len = count - k
-    while (len) {
-        curr = curr.next
-        --len
-    }
-    return curr
+    if (k % length === 0) return head;
+    let newHead = head;
+    for (let i = 1; i < length - k; i++) newHead = newHead.next
+    tail.next = head;
+    head = newHead.next;
+    newHead.next = null
+    return head.next;
 };
 const list = new Node(1, new Node(2, new Node(3, new Node(4))))
-
-console.log(rotateList(list, 3));
+console.log(rotateRight(list, 3));
