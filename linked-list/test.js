822fb50c6da04a415105a4e18ca3b8191c26a44d
moamlrh <moamll288@gmail,com>
2021-03-04 21:58:48 +0300

    test.js

--- linked-list/test.js
+++ linked-list/test.js
@@ -5,20 +5,18 @@ class Node {
     }
 }
 
-var middleNode = function (head) {
-    if (!head) return head;
-    var odd = head;
-    var even = head.next;
-    var evenHead = head.next
-    while (odd.next) {
-        odd.next = even.next
-        odd = odd.next
-        even.next = odd.next
-        even = even.next;
+const func = head => {
+    let curr = head;
+    let temp = curr;
+    let idx = 2;
+    while (curr.next) {
+        if(curr.val === idx-1){
+            curr = curr.next.next
+        }
+        curr = curr.next
     }
-    odd.next = evenHead;
-    return head
-};
-const head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, new Node(6, new Node(7)))))))
+    return head;
+}
 
-console.log(middleNode(head));
+const head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, new Node(6))))))
+console.log(func(head))
