c6074d84eb50f14097d7f6f3a510dffde6df5950
moamlrh <moamll288@gmail,com>
2021-03-07 15:53:59 +0300

    change the method and smpil it by es6 syntax

--- global/reverse-string.js
+++ global/reverse-string.js
@@ -1,8 +1,5 @@
 
-function reverseString(str) {
-    // convert str to array and reverse the array 
-    // and then join to string by sperat all chars in array with nothing => join('')
-    return str.split('').reverse().join('')
-}
+const reverseString = str => str.split('').reverse().join('')
+
 const str = 'Hi my name is Moaml RH'
-console.log(reverseString(str)) // daiR lmaoM si eman ym iH
+console.log(reverseString(str)) 
---
+++ index.js
@@ -0,0 +1,6 @@
+
+function fun(str) {
+    return str.split('').reverse().join('')
+}
+const str = 'Hi my name is Moaml Riad'
+console.log(fun(str))
